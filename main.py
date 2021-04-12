6b85d5a10d46dad82f1e4db839cef3884779357a
skilopsaros <skilopsaros@gmail.com>
2021-04-12 10:52:16 +0300

    more work done

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import numpy as np
 import copy
 # This is the program you are running
 # You can run each energy level many times
-def run(text,ask_decouple,potential,multiply,power):
+def run(text,ask_decouple,potential):
     while input('Run '+text+'? (Y/N) ')=='Y':
         J = int(input('input the value of J: '))
         number_of_drunkards = int(input('input the number of drunkards: '))
-        number_of_runs = int(input('input the number of runs: '))
+        number_of_cycles = int(input('input the number of cycles: '))
         number_to_cut = int(input('input the number to cut: '))
         if ask_decouple:
             decouple = (input('do you want to decouple? (Y/N) ')) # All of these are just inputs to see the parameters you are running with
@@ -22,11 +22,12 @@ def run(text,ask_decouple,potential,multiply,power):
         else:
             decouple = False
 
-        ks, dead = drk.simulation(potential, J, number_of_drunkards, number_of_runs) #This runs the simulation. To see how this runs, go to drunkard.py
+        ks, dead = drk.simulation(potential, J, number_of_drunkards, number_of_cycles) #This runs the simulation. To see how this runs, go to drunkard.py
         ks_copy=copy.deepcopy(ks) #copying things to make sure we don't loose the original data when we alter it
+        xs = list(range(len(ks)))
         for i in range(number_to_cut): #this cuts the first few steps, because the distribution of drunkards is not yet stable
             del(ks[0])
-        xs = range(len(ks))
+            del(xs[0])
         k = np.array(ks)
         if decouple: # to decouple we only need every other point
             ks_decoupled = ks[::2] #use every second value of number of surviving drunkards
@@ -34,32 +35,84 @@ def run(text,ask_decouple,potential,multiply,power):
             xs = xs_decoupled # use the decoupled values
             k = np.array(ks_decoupled)
         m, c, am, ac = slp.best_fit_slope_and_intercept(xs,np.log(k)) #this is a function to find the best fit slope. It works. Find it in slope.py. I tested it with scipy stuff too.
-        print(-m*multiply*pow(J,power))
-        print('plus/minus')
-        print(-am*multiply*pow(J,power)) #this error is too small
+        print(potential(j=J, return_results=True, m=m, am=am))
 
-        print('energy')
-        print((-m*J*J-J)*2/3)
         plt.plot(range(len(dead)),dead) #plot how many died each turn
+        plt.xlabel("Step")
+        plt.ylabel("Walks Terminated")
         plt.show()
         plt.clf()
         plt.subplot(1,2,1) #plot the survivng number every turn
+        plt.xlabel("Step")
+        plt.ylabel("Surviving Drunkards")
         plt.plot(range(len(ks_copy)),ks_copy)
         plt.subplot(1,2,2) #plot the log of that with a fit
+        plt.xlabel("Step")
+        plt.ylabel("Log of surviving Drunkards")
         plt.scatter(xs,np.log(k), s=1, marker='x')
         plt.plot([xs[0],xs[len(xs)-1]],[(m*xs[0]+c),(m*xs[len(xs)-1]+c)])
         plt.show()
         plt.clf()
 
+def run_bare(decouple,potential,J,number_of_drunkards,number_of_cycles,number_to_cut):
+    ks, dead = drk.simulation(potential, J, number_of_drunkards, number_of_cycles)
+    ks_copy=copy.deepcopy(ks)
+    for i in range(number_to_cut):
+        del(ks[0])
+    xs = range(len(ks))
+    k = np.array(ks)
+    if decouple:
+        ks_decoupled = ks[::2]
+        xs_decoupled = xs[::2]
+        xs = xs_decoupled
+        k = np.array(ks_decoupled)
+    m, c, am, ac = slp.best_fit_slope_and_intercept(xs,np.log(k))
+    return(potential(j=J, return_results=True, m=m, am=am))
 
+def run_generation(text,ask_decouple,potential):
+    if input('Run '+text+'? (Y/N) ')=='Y':
+        number_of_runs = int(input('input the number of runs: '))
+        J = int(input('input the value of J: '))
+        number_of_drunkards = int(input('input the number of drunkards: '))
+        number_of_cycles = int(input('input the number of cycles in each run: '))
+        number_to_cut = int(input('input the number to cut: '))
+        if ask_decouple:
+            decouple = (input('do you want to decouple? (Y/N) ')) # All of these are just inputs to see the parameters you are running with
+            if decouple == 'Y':
+                decouple = True
+            else:
+                decouple = False
+        else:
+            decouple = False
+        ms = []
+        for i in range(number_of_runs):
+            print('Run '+str(i+1) +'/'+str(number_of_runs))
+            ms.append(run_bare(decouple,potential,J,number_of_drunkards,number_of_cycles,number_to_cut)[0])
+        ms.sort()
+        ms = np.array(ms)
+        print('mean m')
+        print(np.average(ms))
+        print('standard deviation')
+        print(np.std(ms))
+        plt.hist(ms)
+        plt.show()
 
-run('Energy level 1 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E1,8/pow(maths.pi,2),2)
-run('Energy level 2 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E2,2/pow(maths.pi,2),2)
-run('Energy level 1 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E1,2/pow(maths.pi,2),2)
-run('Energy level 2 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E2,2/pow(maths.pi,2),2)
-run('Energy level 3 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E3,2/pow(maths.pi,2),2)
-run('Energy level 1 of SHO',False,pnt.one_d_SHO_E1,1,1)
-run('Energy level 2 of SHO',False,pnt.one_d_SHO_E2,1,1)
-run('Energy level 1 of Hydrogen',False,pnt.hydrogen_E1,1,0)
-#print('energy')
-#print((-m*J*J-J)*2/3)
+if input('Run Seperate tests? (Y/N) ')== 'Y':
+    run('Energy level 1 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E1)
+    run('Energy level 2 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E2)
+    run('Energy level 1 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E1)
+    run('Energy level 2 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E2)
+    run('Energy level 3 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E3)
+    run('Energy level 1 of SHO',False,pnt.one_d_SHO_E1)
+    run('Energy level 2 of SHO',False,pnt.one_d_SHO_E2)
+    run('Energy level 1 of Hydrogen',False,pnt.hydrogen_E1)
+
+if input('Run entire tests? (Y/N) ')== 'Y':
+    run_generation('Energy level 1 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E1)
+    run_generation('Energy level 2 of 1 dimentional infinite square well',True,pnt.one_d_inf_sqare_well_E2)
+    run_generation('Energy level 1 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E1)
+    run_generation('Energy level 2 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E2)
+    run_generation('Energy level 3 of 2 dimentional infinite circle well',True,pnt.two_d_inf_cyrcle_well_E3)
+    run_generation('Energy level 1 of SHO',False,pnt.one_d_SHO_E1)
+    run_generation('Energy level 2 of SHO',False,pnt.one_d_SHO_E2)
+    run_generation('Energy level 1 of Hydrogen',False,pnt.hydrogen_E1)
--- potentials.py
+++ potentials.py
@@ -1,14 +1,18 @@
 import math as maths
 import random
 
-def one_d_inf_sqare_well_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False): #this is the first potential
+def one_d_inf_sqare_well_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0): #this is the first potential
+    if return_results:
+        return(-m*j*j*8/pow(maths.pi,2),am*j*j*8/pow(maths.pi,2))
     if return_dimensions:
         return(1) #the one dimentional infinite square well has one dimension
     if return_starting_pints:
         return([False])
     return(abs(coordinates[0]) < j) #keep them alive only if their coordinate is lower than J
 
-def one_d_inf_sqare_well_E2(return_dimensions=False, coordinates=[0], j=8, return_starting_pints = False):
+def one_d_inf_sqare_well_E2(return_dimensions=False, coordinates=[0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return(-m*j*j*2/pow(maths.pi,2),am*j*j*2/pow(maths.pi,2))
     if return_dimensions:
         return(1)
     if return_starting_pints:
@@ -17,14 +21,18 @@ def one_d_inf_sqare_well_E2(return_dimensions=False, coordinates=[0], j=8, retur
         return(False)
     return(abs(coordinates[0]) < j)
 
-def two_d_inf_cyrcle_well_E1(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False):
+def two_d_inf_cyrcle_well_E1(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return((-m*j*j),(am*j*j))
     if return_dimensions:
         return(2)
     if return_starting_pints:
         return([False])
     return(pow(abs(coordinates[0]),2)+pow(abs(coordinates[1]),2) < pow(j,2))
 
-def two_d_inf_cyrcle_well_E2(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False):
+def two_d_inf_cyrcle_well_E2(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return(-m*j*j*2/pow(maths.pi,2),am*j*j*2/pow(maths.pi,2))
     if return_dimensions:
         return(2)
     if return_starting_pints:
@@ -33,7 +41,9 @@ def two_d_inf_cyrcle_well_E2(return_dimensions=False, coordinates=[0,0], j=8, re
         return(False)
     return(pow(abs(coordinates[0]),2)+pow(abs(coordinates[1]),2) < pow(j,2))
 
-def two_d_inf_cyrcle_well_E3(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False):
+def two_d_inf_cyrcle_well_E3(return_dimensions=False, coordinates=[0,0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return(-m*j*j*2/pow(maths.pi,2),am*j*j*2/pow(maths.pi,2))
     if return_dimensions:
         return(2)
     if return_starting_pints:
@@ -42,14 +52,18 @@ def two_d_inf_cyrcle_well_E3(return_dimensions=False, coordinates=[0,0], j=8, re
         return(False)
     return(pow(abs(coordinates[0]),2)+pow(abs(coordinates[1]),2) < pow(j,2))
 
-def one_d_SHO_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False):
+def one_d_SHO_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return(-m*j,am*j)
     if return_dimensions:
         return(1)
     if return_starting_pints:
         return([False])
     return(random.random()>pow(coordinates[0],2)/(2*j*j))
 
-def one_d_SHO_E2(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False):
+def one_d_SHO_E2(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return(-m*j,am*j)
     if return_dimensions:
         return(1)
     if return_starting_pints:
@@ -59,7 +73,9 @@ def one_d_SHO_E2(return_dimensions=False, coordinates = [0], j=8, return_startin
     return(random.random()>pow(coordinates[0],2)/(2*j*j))
 
 
-def hydrogen_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False):
+def hydrogen_E1(return_dimensions=False, coordinates = [0], j=8, return_starting_pints = False, return_results = False, m=0, am = 0):
+    if return_results:
+        return((-m*j*j-j)*2/3)
     if return_dimensions:
         return(3)
     if return_starting_pints:
